92d07680d0dbb2805133ac6e7f7281f6d67fedb8
Maxwell <maxwell@rgnt2-108-95-dhcp.int.colorado.edu>
2022-04-25 09:21:38 -0600

    Basic label change code from youtube example

---
+++ driver.py
@@ -0,0 +1,22 @@
+import sys
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QApplication, QDialog
+from PyQt5.uic import loadUi
+
+class main_page(QDialog):
+    def __init__(self):
+        super(main_page, self).__init__()
+        loadUi('Firstpage.ui', self)
+        self.pushButton.clicked.connect(self.setText)
+
+    def setText(self):
+        tbs = self.plainTextEdit.toPlainText()
+        self.label_4.setText(str(tbs))
+
+
+
+
+app = QApplication(sys.argv)
+widget = main_page()
+widget.show()
+sys.exit(app.exec_())
